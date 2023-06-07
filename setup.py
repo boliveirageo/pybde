cb6fbef04fb704338ec4a304f94a86f8e14d1faa
boliveirageo <so_geoprocessamento@yahoo.com.br>
2023-06-07 14:02:14 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='pybde',
       version='0.1',
-      description='A module to access data from the Mauro Borges Statistic and Socioeconomic Institute (IMB), Goiás - Brazil ',
+      description='A module to access data from the Mauro Borges Statistic and Socioeconomic Institute (IMB), Goias - Brazil ',
       url='https://github.com/boliveirageo/pybde',
       author = 'Bernard Silva de Oliveira',
       author_email = 'bernard.oliveira@goias.gov.br         ',
