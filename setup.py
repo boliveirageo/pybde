522a2a4a276ac00e0b1f0b347797824462255a49
boliveirageo <so_geoprocessamento@yahoo.com.br>
2023-06-07 13:59:50 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='pybde',
+      version='0.1',
+      description='A module to access data from the Mauro Borges Statistic and Socioeconomic Institute (IMB), Goiás - Brazil ',
+      url='https://github.com/boliveirageo/pybde',
+      author = 'Bernard Silva de Oliveira',
+      author_email = 'bernard.oliveira@goias.gov.br         ',
+      license='MIT',
+      packages=['pybde'],
+      zip_safe=False,
+      install_requires=['requests', 'json'],
+      )
